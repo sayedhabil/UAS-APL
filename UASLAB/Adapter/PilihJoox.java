2fbfb77ab95eba18247ef2762fa7d50528c0ad3a
sayedhabil <74532540+sayedhabil@users.noreply.github.com>
2021-12-09 23:59:29 +0700

    Update PilihJoox.java

--- UASLAB/Adapter/PilihJoox.java
+++ UASLAB/Adapter/PilihJoox.java
@@ -2,13 +2,13 @@ package Adapter;
 
 public class PilihJoox implements ThePlayer{
 
-   //Method yang akan mengoverride method yang ada pada class AdvancedMediaPlayer
+   //Untuk meng-override method pada ThePlayer
    @Override
    public void mainkanJoox(String namaLagu) {
       System.out.println( namaLagu + "diputar dengan Joox");		
    }
 
-   //Method yang akan mengoverride method yang ada pada class AdvancedMediaPlayer
+   //Override akan memproses method yang ada pada class ThePlayer
    @Override
    public void mainkanSpotify(String namaLagu) {
    }
