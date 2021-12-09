504fa59e7166e09f3e9a099ad0ed8e972cbffc1f
sayedhabil <74532540+sayedhabil@users.noreply.github.com>
2021-12-09 23:57:27 +0700

    Update MediaSound.java

--- UASLAB/Adapter/MediaSound.java
+++ UASLAB/Adapter/MediaSound.java
@@ -2,6 +2,6 @@ package Adapter;
 
 public interface MediaSound {
    
-   //method yang akan di overide pada class class yang diperlukan
+   //method yang akan di overide pada class yang diperlukan
    public void play(String soundSystem, String namaLagu);
 }
