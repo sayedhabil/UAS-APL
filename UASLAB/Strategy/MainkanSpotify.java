004de5aa47204845e50a58d63523b663181393f9
sayedhabil <74532540+sayedhabil@users.noreply.github.com>
2021-12-10 00:05:15 +0700

    Update MainkanSpotify.java

--- UASLAB/Strategy/MainkanSpotify.java
+++ UASLAB/Strategy/MainkanSpotify.java
@@ -1,9 +1,9 @@
 package Strategy;
 
-//Class yang akan mengimplementasi class PlayerMethod
+//Class untuk mengimplementasi class PlayerMethod
 public class MainkanSpotify implements PlayerMethod{
 
-    //Implementasi method yang ada pada PlayerMethod
+    //Untuk mengimplementasi method yang ada pada PlayerMethod
     public void mainkanMusik(){
         System.out.println("Music diputar denagn Spotify");
     }
